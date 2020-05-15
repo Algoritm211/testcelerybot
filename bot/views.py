90d71df717decac948efe27fb3a7eab73fcdf2a5
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-15 20:26:19 +0300

    hosts add

--- bot/views.py
+++ bot/views.py
@@ -109,7 +109,10 @@ def set_crypto(message):
         for i in check:
             text += i + '\n'
         info = User.objects.get(user_id=message.chat.id)
-        info.coins = check
+        text = ''
+        for i in check:
+            text += i
+        info.coins = text
         info.save()
         bot.send_message(message.chat.id, text, reply_markup=keyboard_1)
     else:
