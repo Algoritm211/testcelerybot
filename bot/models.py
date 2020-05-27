104dba0604a80bdc684502d1204ef2f6894bf210
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-28 00:19:39 +0300

    celery

--- bot/models.py
+++ bot/models.py
@@ -5,8 +5,8 @@ from django.db import models
 class User(models.Model):
     user_id = models.CharField(max_length=200, unique=True)
     date_pubs = models.DateTimeField(auto_now_add=True)
-    # coins = models.TextField(blank=True)
-    # send_daily_prices = models.BooleanField(default=False)
+    coins = models.TextField(blank=True)
+    send_daily_prices = models.BooleanField(default=False)
 
     def __str__(self):
         return self.user_id, self.date_pubs, self.coins
