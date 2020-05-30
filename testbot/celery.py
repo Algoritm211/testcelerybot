e8fba4cf4ba73ea1a57136130ce312f045aa2c8a
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-30 11:36:10 +0300

    celery

--- testbot/celery.py
+++ testbot/celery.py
@@ -1,12 +1,13 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
+from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
 
 app = Celery('testbot')
 app.config.from_object('django.conf:settings', namespace='CELERY')
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 #celery beat task
 
