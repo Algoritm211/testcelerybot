d85683b7db253c1c3adb82bf9d63779628c23e3b
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-30 12:14:15 +0300

    celery

--- testbot/celery.py
+++ testbot/celery.py
@@ -1,7 +1,7 @@
 import os
 from celery import Celery
 from celery.schedules import crontab
-from django.conf import settings
+# from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
 
@@ -17,7 +17,3 @@ app.conf.beat = {
         'schedule': crontab('*/3')
     }
 }
-
-# REDIS_URL = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
-# app.conf.update(BROKER_URL=os.environ[REDIS_URL],
-#                 CELERY_RESULT_BACKEND=os.environ[REDIS_URL])
