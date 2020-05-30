e6cdcfe8f6d7e2d9513d443ada6c89cd16e80901
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-30 09:53:50 +0300

    celery

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,2 +1,8 @@
 from testbot.celery import app
 from .models import User
+from .views import send_daily_cryptos
+
+
+@app.task
+def send_daily_cryptocurrency():
+    send_daily_cryptos()
--- testbot/celery.py
+++ testbot/celery.py
@@ -1,5 +1,6 @@
 import os
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
 
@@ -8,3 +9,10 @@ app.config.from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks()
 
 #celery beat task
+
+app.conf.beat = {
+    'send-daily-crypto': {
+        'task': 'bot.tasks.send_daily_cryptocurrency',
+        'schedule': crontab('*/3')
+    }
+}
