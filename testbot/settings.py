d81cf1d3c5375efc863264a7d0cd4c1f717cc67b
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-05-31 09:18:48 +0300

    celery

--- testbot/settings.py
+++ testbot/settings.py
@@ -24,7 +24,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # SECURITY WARNING: keep the secret key used in production secret!
 
 SECRET_KEY = '$3_+k-_na_3b(v*$k-ezss#9c@^2dyi!4-flc(e&q=#5keref4'
-# SECRET_KEY = os.environ.get('$3_+k-_na_3b(v*$k-ezss#9c@^2dyi!4-flc(e&q=#5keref4', '')
+
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
