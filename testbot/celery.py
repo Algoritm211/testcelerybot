480eb0e4c8a7b0200f7e055838a042bbc56a6773
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-01 00:32:44 +0300

    celery

--- testbot/celery.py
+++ testbot/celery.py
@@ -9,7 +9,7 @@ from django.conf import settings
 os.environ['DJANGO_SETTINGS_MODULE'] = "testbot.settings"
 
 app = Celery('testbot')
-app.config.from_object('django.conf:settings', namespace='CELERY')
+# app.config.from_object('django.conf:settings', namespace='CELERY')
 app.conf.update(BROKER_URL=os.environ['redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'],
                 CELERY_RESULT_BACKEND=os.environ['redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'])
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
