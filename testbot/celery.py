c96d37e3f61cd66d15320969c60d903e4b9b38a1
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-01 23:52:39 +0300

    celery

--- testbot/celery.py
+++ testbot/celery.py
@@ -6,13 +6,13 @@ from django.conf import settings
 # from . import celeryconfig
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
+os.environ.setdefault('REDIS_URL', 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059')
 # os.environ['DJANGO_SETTINGS_MODULE'] = "testbot.settings"
-# os.environ.setdefault('REDIS_URL', failobj)
 app = Celery('testbot')
 # app.config.from_object('django.conf:settings', namespace='CELERY')
-redis_url = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
-app.conf.update(BROKER_URL=os.environ[redis_url],
-                CELERY_RESULT_BACKEND=os.environ[redis_url])
+# redis_url = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'])
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # celery beat task
