5dcca3cdc881bab9b1eae7c2271af0f158f55e91
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-02 13:41:08 +0300

    celery

--- testbot/settings.py
+++ testbot/settings.py
@@ -124,6 +124,7 @@ STATIC_URL = '/static/'
 
 
 # redis settings for celery
+
 REDIS_URL = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
 r = redis.from_url(os.environ.get(REDIS_URL))
 # redis_url = urlparse.urlparse(os.environ.get(REDIS_URL))
@@ -135,15 +136,9 @@ CACHES = {
         }
     }
 
-# BROKER_URL = REDIS_URL
-
-# CELERY_BROKER_URL = REDIS_URL
-# CELERY_RESULT_BACKEND = REDIS_URL
-# # REDIS_HOST = 'ec2-3-220-244-30.compute-1.amazonaws.com'
-# # REDIS_PORT = '14059'
-# # REDIS_PASSWORD = 'p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792'
+CELERY_BROKER_URL = REDIS_URL
+CELERY_RESULT_BACKEND = REDIS_URL
 CELERY_BROKER_TRANSPORT_OPTIONS = {'visibility_timeout': 3600}
-# CELERY_RESULT_BACKEND = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
 CELERY_ACCEPT_CONTENT = ['application/json']
 CELERY_TASK_SERIALIZER = 'json'
 CELERY_RESULT_SERIALIZER = 'json'
