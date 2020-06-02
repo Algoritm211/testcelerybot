c707585fbeb677f7da1471da0ecae148b1fddc1e
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-02 14:50:13 +0300

    celery

--- testbot/celery.py
+++ testbot/celery.py
@@ -5,7 +5,8 @@ from celery.schedules import crontab
 from django.conf import settings
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
-os.environ.setdefault('REDIS_URL', 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059')
+os.environ.setdefault('REDIS_URL',
+                      'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059')
 
 app = Celery('testbot')
 app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
@@ -15,9 +16,15 @@ app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # celery beat task
 
+
 app.conf.beat = {
     'send-daily-crypto': {
         'task': 'bot.tasks.send_daily_cryptocurrency',
         'schedule': crontab(),
     },
 }
+
+
+@app.task(bind=True)
+def debug_task(self):
+    print('Request: {0!r}'.format(self.request))
