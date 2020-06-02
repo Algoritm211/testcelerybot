5579dc04b00c229b55765a87dfb8dbae497497c6
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-02 13:14:40 +0300

    celery

--- testbot/settings.py
+++ testbot/settings.py
@@ -125,7 +125,7 @@ STATIC_URL = '/static/'
 
 # redis settings for celery
 REDIS_URL = 'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059'
-# r = redis.from_url(os.environ.get(REDIS_URL))
+r = redis.from_url(os.environ.get(REDIS_URL))
 # redis_url = urlparse.urlparse(os.environ.get(REDIS_URL))
 
 CACHES = {
