5629fa74f7714dc1f13f6e911be8b41e705604da
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-02 12:03:33 +0300

    celery

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,11 +1,15 @@
 from testbot.celery import app
 from .models import User
+import telebot
+
+TOKEN = '1113179664:AAEaV5nToFyEdoOAF5NrhjjncnLCJKbHXGs'
+bot = telebot.TeleBot(TOKEN)
 
 
 @app.task
 def send_daily_cryptocurrency():
-    print('WOW')
-    # all_users = User.objects.all()
-    # for user in all_users:
-    #     if not user.send_daily_prices:
-    #         bot.send_message(user.user_id, 'У не вас стоит напоминание')
+    # print('WOW')
+    all_users = User.objects.all()
+    for user in all_users:
+        if not user.send_daily_prices:
+            bot.send_message(user.user_id, 'У не вас стоит напоминание')
