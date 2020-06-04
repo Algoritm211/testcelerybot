a1cc080597e1224974da1c17ce5a0fa1e7012d71
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-04 14:19:24 +0300

    celerybeat

--- testbot/celery.py
+++ testbot/celery.py
@@ -9,14 +9,16 @@ os.environ.setdefault('REDIS_URL',
                       'redis://h:p5067e3205757872a84ea31d841e6cf3ce88f7fcb568d463ff4dc1708d8f8c792@ec2-3-220-244-30.compute-1.amazonaws.com:14059')
 
 app = Celery('testbot')
-# app.conf.update(
-#                 )
-app.config_from_object('django.conf:settings', namespace='CELERY')
+app.conf.update(BROKER_URL=os.environ['REDIS_URL'],
+                CELERY_RESULT_BACKEND=os.environ['REDIS_URL'],
+                CELERY_ACCEPT_CONTENT=['json'],
+                CELERY_TASK_SERIALIZER='json',
+                CELERY_RESULT_SERIALIZER='json'
+                )
+# app.config_from_object('django.conf:settings', namespace='CELERY')
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # celery beat task
-
-
 app.conf.beat_schedule = {
     'send-daily-crypto': {
         'task': 'bot.tasks.send_daily_cryptocurrency',
