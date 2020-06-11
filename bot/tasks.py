9d873b32bdce6d06f84834e1737039ee7e1f27cd
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 19:07:26 +0300

    testbot

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,5 +1,4 @@
 from datetime import timedelta
-from celery.schedules import crontab
 
 from celery.task import periodic_task
 
@@ -13,19 +12,10 @@ TOKEN = '1113179664:AAEaV5nToFyEdoOAF5NrhjjncnLCJKbHXGs'
 bot = telebot.TeleBot(TOKEN)
 
 
-@periodic_task(run_every=timedelta(minutes=2))
+@periodic_task(run_every=timedelta(minutes=1))
 def send_daily_cryptocurrency():
     all_users = User.objects.all()
     text = coinmarket.get_top_10()
     for user in all_users:
-        if user.send_daily_prices:
+        if not user.send_daily_prices:
             bot.send_message(user.user_id, text, parse_mode='HTML')
-
-
-# app.conf.beat_schedule = {
-#     'task-name': {
-#         'task': 'tasks.send_daily_cryptocurrency',
-#         'schedule': crontab(minute='*/3')
-#     },
-# }
-# app.conf.timezone = 'Europe/Kiev'
