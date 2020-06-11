d83c5bbcda74ed683d82d5ebb93026d2ab7eb79b
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 18:30:35 +0300

    testbot

--- testbot/celery.py
+++ testbot/celery.py
@@ -7,8 +7,8 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
 app = Celery('testbot')
 app.config_from_object('django.conf:settings')
-app.conf.timezone = 'UTC'
-app.conf.enable_utc = True
+# app.conf.timezone = 'UTC'
+# app.conf.enable_utc = True
 app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
 
 # app.conf.celery_beat = {
