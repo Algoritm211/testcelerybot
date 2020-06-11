6d63dac6ab698c29c760e31b048b740105e652ae
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 21:20:43 +0300

    testbot

--- bot/tasks.py
+++ bot/tasks.py
@@ -12,7 +12,7 @@ TOKEN = '1113179664:AAEaV5nToFyEdoOAF5NrhjjncnLCJKbHXGs'
 bot = telebot.TeleBot(TOKEN)
 
 
-@periodic_task(run_every=crontab(hour=21, minute=15))
+@periodic_task(run_every=crontab(minute=5))
 def send_daily_cryptocurrency():
     all_users = User.objects.all()
     text = coinmarket.get_top_10()
