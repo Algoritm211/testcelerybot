367270eff1c1ce31899459a308838188731ef880
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 21:14:24 +0300

    testbot

--- bot/tasks.py
+++ bot/tasks.py
@@ -1,7 +1,7 @@
 from datetime import timedelta
 
 from celery.task import periodic_task
-from celery.schedules import crontab
+from celery.task.schedules import crontab
 from testbot.celery import app
 from .models import User
 import telebot
@@ -12,7 +12,7 @@ TOKEN = '1113179664:AAEaV5nToFyEdoOAF5NrhjjncnLCJKbHXGs'
 bot = telebot.TeleBot(TOKEN)
 
 
-@periodic_task(run_every=crontab(hour=21, minute=10))
+@periodic_task(run_every=crontab(hour=21, minute=15))
 def send_daily_cryptocurrency():
     all_users = User.objects.all()
     text = coinmarket.get_top_10()
