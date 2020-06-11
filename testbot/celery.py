e3f6b012030012b093e46e484679dc52264bbde6
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 20:02:22 +0300

    testbot

--- testbot/celery.py
+++ testbot/celery.py
@@ -7,9 +7,9 @@ from django.conf import settings
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'testbot.settings')
 app = Celery('testbot')
 app.config_from_object('django.conf:settings')
-app.conf.timezone = 'UTC'
-app.conf.enable_utc = True
-app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
+app.conf.timezone = 'Europe/Kiev'
+app.autodiscover_tasks()
+
 
 # app.conf.celery_beat = {
 #     'send-daily-crypto': {
