675659d9a18e5711023fd65794a0fa84c267ba59
Algoritm211 <60394886+Algoritm211@users.noreply.github.com>
2020-06-11 15:07:58 +0300

    celerybeat

--- bot/views.py
+++ bot/views.py
@@ -207,12 +207,11 @@ def inline_buttons(call):
             bot.send_message(call.message.chat.id, 'Команда 2348', parse_mode='HTML')
         elif call.data == 'set_notif_off':
             user_data_for_set_on = User.objects.get(user_id=call.message.chat.id)
-            user_data_for_set_on.send_daily_prices = 'false'
+            user_data_for_set_on.send_daily_prices = 'f'
             user_data_for_set_on.save()
             bot.answer_callback_query(callback_query_id=call.id, text='Уведомления выключены')
-        elif call.data == 'send_notif_on':
+        elif call.data == 'set_notif_on':
             print('AAAAAAAAAAAAAAAAAAAAAAAAAA')
-            
             user_data_for_set_off = User.objects.get(user_id=call.message.chat.id)
             user_data_for_set_off.send_daily_prices = 'true'
             user_data_for_set_off.save()
